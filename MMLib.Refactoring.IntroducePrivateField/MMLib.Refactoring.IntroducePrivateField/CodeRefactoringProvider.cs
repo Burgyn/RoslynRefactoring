2a12ce4e03270b0f06745a7d4a9f9b2a727f5d68
Milan Martiniak <mino.martiniak@gmail.com>
2017-03-30 07:10:51 +0200

    Create projects

--- MMLib.Refactoring.IntroducePrivateField/MMLib.Refactoring.IntroducePrivateField/CodeRefactoringProvider.cs
+++ MMLib.Refactoring.IntroducePrivateField/MMLib.Refactoring.IntroducePrivateField/CodeRefactoringProvider.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Composition;
 using System.Linq;
 using System.Threading;
@@ -8,54 +7,111 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.Formatting;
+using System;
 
 namespace MMLib.Refactoring.IntroducePrivateField
 {
-    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(MMLibRefactoringIntroducePrivateFieldCodeRefactoringProvider)), Shared]
-    internal class MMLibRefactoringIntroducePrivateFieldCodeRefactoringProvider : CodeRefactoringProvider
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(IntroducePrivateFieldCodeRefactoringProvider)), Shared]
+    internal class IntroducePrivateFieldCodeRefactoringProvider : CodeRefactoringProvider
     {
         public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each refactoring to offer
-
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-
-            // Find the node at the selection.
+            var root = await context.Document
+                .GetSyntaxRootAsync(context.CancellationToken)
+                .ConfigureAwait(false);
             var node = root.FindNode(context.Span);
-
-            // Only offer a refactoring if the selected node is a type declaration node.
-            var typeDecl = node as TypeDeclarationSyntax;
-            if (typeDecl == null)
+            var parameter = node as ParameterSyntax;
+            if (parameter == null)
             {
                 return;
             }
 
-            // For any type declaration node, create a code action to reverse the identifier text.
-            var action = CodeAction.Create("Reverse type name", c => ReverseTypeNameAsync(context.Document, typeDecl, c));
+            var parameterName = RoslynHelpers.GetParameterName(parameter);
+            var underscorePrefix = $"_{parameterName}";
+            var uppercase = $"{parameterName.Substring(0, 1).ToUpper()}{parameterName.Substring(1)}";
 
-            // Register this code action.
+            if (RoslynHelpers.VariableExists(root, parameterName, underscorePrefix, uppercase))
+            {
+                return;
+            }
+            var isReferenceType = await IsReferenceType(context, parameter);
+
+            var action = CodeAction.Create(
+                $"Introduce as private field '{underscorePrefix}'",
+                ct =>
+                CreateFieldAsync(context, parameter, parameterName, underscorePrefix, ct, isReferenceType));
+
             context.RegisterRefactoring(action);
         }
 
-        private async Task<Solution> ReverseTypeNameAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private static async Task<bool> IsReferenceType(CodeRefactoringContext context, ParameterSyntax parameter)
         {
-            // Produce a reversed version of the type declaration's identifier token.
-            var identifierToken = typeDecl.Identifier;
-            var newName = new string(identifierToken.Text.ToCharArray().Reverse().ToArray());
+            var semantic = await context.Document.GetSemanticModelAsync(context.CancellationToken);
+            var referenceType = semantic.GetTypeInfo(parameter.Type).Type.IsReferenceType;
 
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+            return referenceType;
+        }
 
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+        private async Task<Document> CreateFieldAsync(
+            CodeRefactoringContext context,
+            ParameterSyntax parameter,
+            string paramName, string fieldName,
+            CancellationToken cancellationToken,
+            bool isReferenceType)
+        {
+            var oldConstructor = parameter
+                                 .Ancestors()
+                                 .OfType<ConstructorDeclarationSyntax>()
+                                 .First();
 
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
+            ExpressionSyntax expression = CreateAssignmentExpression(paramName, isReferenceType);
+
+            var newConstructor =
+                oldConstructor
+                .WithBody(
+                    oldConstructor.Body.AddStatements(
+                        SyntaxFactory.ExpressionStatement(
+                            SyntaxFactory.AssignmentExpression(
+                                SyntaxKind.SimpleAssignmentExpression,
+                                SyntaxFactory.IdentifierName(fieldName),
+                                expression))));
+
+            var oldClass = parameter.FirstAncestorOrSelf<ClassDeclarationSyntax>();
+            var oldClassWithNewCtor = oldClass.ReplaceNode(oldConstructor, newConstructor);
+
+            var fieldDeclaration = RoslynHelpers.CreateFieldDeclaration(RoslynHelpers.GetParameterType(parameter), fieldName);
+            var newClass = oldClassWithNewCtor
+                .WithMembers(oldClassWithNewCtor.Members.Insert(0, fieldDeclaration))
+                .WithAdditionalAnnotations(Formatter.Annotation);
+
+            var oldRoot = await context.Document
+                .GetSyntaxRootAsync(cancellationToken)
+                .ConfigureAwait(false);
+            var newRoot = oldRoot.ReplaceNode(oldClass, newClass);
+
+            return context.Document.WithSyntaxRoot(newRoot);
+        }
+
+        private static ExpressionSyntax CreateAssignmentExpression(string paramName, bool isReferenceType)
+        {
+            var throwStatement = SyntaxFactory.ThrowStatement(
+               SyntaxFactory.ParseExpression($" throw new {nameof(ArgumentNullException)}(nameof({paramName}))"));
+
+            ExpressionSyntax expression = null;
+            if (isReferenceType)
+            {
+                expression = SyntaxFactory.BinaryExpression(
+                    SyntaxKind.CoalesceExpression,
+                    SyntaxFactory.IdentifierName(paramName),
+                    throwStatement.Expression);
+            }
+            else
+            {
+                expression = SyntaxFactory.IdentifierName(paramName);
+            }
+
+            return expression;
         }
     }
 }
---
+++ MMLib.Refactoring.IntroducePrivateField/MMLib.Refactoring.IntroducePrivateField/RoslynHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMLib.Refactoring.IntroducePrivateField
+{
+    public static class RoslynHelpers
+    {
+        public static bool VariableExists(SyntaxNode root, params string[] variableNames)
+        {
+            return root
+                .DescendantNodes()
+                .OfType<VariableDeclarationSyntax>()
+                .SelectMany(ps => ps.DescendantTokens().Where(t => t.IsKind(SyntaxKind.IdentifierToken) && variableNames.Contains(t.ValueText)))
+                .Any();
+        }
+
+        public static string GetParameterType(ParameterSyntax parameter)
+        {
+            return parameter
+                .DescendantNodes()
+                .First(node => node is PredefinedTypeSyntax || node is IdentifierNameSyntax)
+                .GetFirstToken()
+                .ValueText;
+        }
+
+        public static string GetParameterName(ParameterSyntax parameter)
+        {
+            return parameter.DescendantTokens().Where(t => t.IsKind(SyntaxKind.IdentifierToken)).Last().ValueText;
+        }
+
+        public static FieldDeclarationSyntax CreateFieldDeclaration(string type, string name)
+        {
+            return SyntaxFactory.FieldDeclaration(
+                SyntaxFactory.VariableDeclaration(SyntaxFactory.IdentifierName(type))
+                .WithVariables(SyntaxFactory.SingletonSeparatedList(SyntaxFactory.VariableDeclarator(SyntaxFactory.Identifier(name)))))
+                .WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(SyntaxKind.PrivateKeyword), SyntaxFactory.Token(SyntaxKind.ReadOnlyKeyword)));
+        }
+    }
+}
